2a37b22ea823e41c70a58e65695b89f1dca428d2
chunkityip <alex0917lfo@hotmail.com>
2024-10-07 06:16:01 -0400

    feat: implement UserService with 5 methods - registerUser() -loginUser() -getAllUser() -getLoginUser() -getUserInfoAndOrderHistory()

---
+++ Backend/src/main/java/com/example/E_Commerce/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.E_Commerce.service;
+
+import com.example.E_Commerce.dto.LoginRequest;
+import com.example.E_Commerce.dto.Response;
+import com.example.E_Commerce.dto.UserDto;
+import com.example.E_Commerce.entity.User;
+
+public interface UserService {
+
+    Response registerUser(UserDto registrationRequest);
+
+    Response loginUser(LoginRequest loginRequest);
+
+    Response getAllUser(UserDto registrationRequest);
+
+    User getLoginUser();
+
+    Response getUserInfoAndOrderHistory();
+
+
+}
---
+++ Backend/src/main/java/com/example/E_Commerce/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.E_Commerce.service.impl;
+
+import com.example.E_Commerce.dto.LoginRequest;
+import com.example.E_Commerce.dto.Response;
+import com.example.E_Commerce.dto.UserDto;
+import com.example.E_Commerce.entity.User;
+import com.example.E_Commerce.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public Response registerUser(UserDto registrationRequest) {
+        return null;
+    }
+
+    @Override
+    public Response loginUser(LoginRequest loginRequest) {
+        return null;
+    }
+
+    @Override
+    public Response getAllUser(UserDto registrationRequest) {
+        return null;
+    }
+
+    @Override
+    public User getLoginUser() {
+        return null;
+    }
+
+    @Override
+    public Response getUserInfoAndOrderHistory() {
+        return null;
+    }
+}
