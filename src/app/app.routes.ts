7e05ebaecbc5ce8258dade83a0f055b55be54b57
Chunkit Yip <chunkit.yip@peopleshores.com>
2024-10-23 09:06:28 -0400

    merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
+  /**PUBLIC ROUTES */
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'cart', component: CartComponent},
+
+  /**WIDE CARD */
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '**', redirectTo: '/home'},
 ];
