2f9bacf40516440fbd9687796d6895efd0dbc0fe
Chunkit Yip <chunkit.yip@peopleshores.com>
2024-10-24 16:25:24 -0400

    feat: implement Category component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,17 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ProductdetailsComponent } from './productdetails/productdetails.component';
+import { CategoryComponent } from './category/category.component';
+
 
 export const routes: Routes = [
   /**PUBLIC ROUTES */
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'product/:productId', component: ProductdetailsComponent},
+  {path: 'categories', component: CategoryComponent},
   {path: 'cart', component: CartComponent},
 
   /**USERS ROUTES */
